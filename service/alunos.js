091a59165e954314dc52556cbf39a34dc84b136e
João Marcuz <126777006+Marcuz015@users.noreply.github.com>
2024-02-29 15:11:38 -0300

    Update alunos.js

--- service/alunos.js
+++ service/alunos.js
@@ -1,9 +1,11 @@
 import { alunos } from "../data/data.js";
 
+//Retorna os alunos
 export const listar = ()=>{
     return alunos;
 }
 
+//Chama os alunos por ID
 export const consultarPorId = (id)=>{
     return alunos.find(aluno => aluno.id == id);
 }
